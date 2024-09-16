953ed59f7135f1794a11fe5f4e56978941b96341
Luke Rushton <luke.rushtonx@gmail.com>
2024-09-16 10:54:46 -0700

    added links to students with same taxonomies

--- single-fwd-student.php
+++ single-fwd-student.php
@@ -16,18 +16,39 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			//maybe remove this?
 			get_template_part( 'template-parts/content', get_post_type() );
+			$post_id = get_the_ID();
+			//update the cta based on taxonomy
+			$speciality = join(', ', wp_list_pluck(get_the_terms($post_id, 'fwd-program'), 'slug')); 
+			echo '<h3>Meet other '. $speciality . ' students:</h3>';
 
-			//update this based on taxonomy
-			echo '<h3>[Meet the other Designer Students]</h3>';
+			//grab all the students of matching terms
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( '', 'school-theme' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( '', 'school-theme' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+			$args = array(
+							'post_type'      => 'fwd-student',
+							'posts_per_page' => -1,
+							'orderby' 		 => 'title',
+							'order' 		 => 'ASC',
+							'tax_query' 	 => array(
+								array(
+									'taxonomy' =>'fwd-program',
+									'field' => 'slug',
+									'terms' => $speciality
+								)
+							)
+						);
+			$query = new WP_Query( $args );
+			if ( $query -> have_posts() ){
+				while ( $query -> have_posts() ) {
+					$query -> the_post();
+					if(get_the_ID() != $post_id){
+						echo "<a href='". get_the_permalink() ."'>";
+						the_title();
+						echo "</a>";
+					}
+						wp_reset_postdata();
+				} 
+			}
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
